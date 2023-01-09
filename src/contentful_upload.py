eb312f6363beaf13056010b727826b6e8ebe7a5b
Half Scheidl <contact@hscheidl.com>
2023-01-09 21:28:16 +0100

    Migrating

--- src/contentful_upload.py
+++ src/contentful_upload.py
@@ -5,16 +5,18 @@ from contentful_management import Client
 import argparse
 import sys
 import os
+import json
 import subprocess
 from pathlib import Path
+import frontmatter
 
 
-#Works only on MacOS
+# Works only on MacOS
 def getKeyChain(s):
-    cmd=['security','find-generic-password','-s',s,'-w']
-    res=subprocess.run(cmd, capture_output=True, text=True)
+    cmd = ['security', 'find-generic-password', '-s', s, '-w']
+    res = subprocess.run(cmd, capture_output=True, text=True)
     if res.stderr:
-         raise Exception(res.stderr)
+        raise Exception(res.stderr)
     else:
         return res.stdout
 
@@ -27,7 +29,7 @@ print("Using secrets from MacOS Key Chain. Contentful Space ID: " + CONTENTFUL_S
 
 
 # Other restrictions
-EXT_ALLOWED=['.png','.jpeg','.jpg']
+EXT_ALLOWED = ['.png', '.jpeg', '.jpg', '.md']
 
 
 parser = argparse.ArgumentParser(description='Uploads files to Contentful')
@@ -37,7 +39,7 @@ parser.add_argument('-t', '--title', type=str,
                     help='Title to be assigned to the assets', required=True)
 
 parser.add_argument('-m', '--markdown', type=str,
-                    help='A file containing markdown text to be used in the description field.', required=True)
+                    help='A file containing markdown text to be used in the description field.', required=False)
 
 args = parser.parse_args()
 
@@ -64,7 +66,26 @@ if (mode == MODE_FILE and not os.path.exists(file)):
 client = Client(CONTENTFUL_API_KEY)
 space = client.spaces().find(CONTENTFUL_SPACE_ID)
 
-def uploadAsset(filepath,title):
+
+def uploadContent(title, content):
+    attribs = {
+        "content_type_id": "post",
+        "fields": {
+            "title": {
+                "en-US": title
+            },
+            "testingMd": {
+                "en-US": content
+            }
+        }
+    }
+
+    entry = client.entries(CONTENTFUL_SPACE_ID, CONTENTFUL_ENV).create(
+        resource_id=None, attributes=attribs)
+    return entry
+
+
+def uploadAsset(filepath, title):
     fileName = os.path.basename(filepath)
     print(f"Uploading: {fileName}")
 
@@ -74,45 +95,59 @@ def uploadAsset(filepath,title):
     uploadId = upload.id
 
     asset = client.assets(CONTENTFUL_SPACE_ID, CONTENTFUL_ENV).create(None,
-                                            {
-                                                "fields": {
-                                                    "title": {
-                                                        "en-US": title
-                                                    },
-                                                    "file": {
-                                                        "en-US": {
-                                                            "contentType": "image/png",
-                                                            "fileName": fileName,
-                                                            "uploadFrom": {
-                                                                "sys": {
-                                                                    "type": "Link",
-                                                                    "linkType": "Upload",
-                                                                    "id": uploadId
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            )
+                                                                      {
+                                                                          "fields": {
+                                                                              "title": {
+                                                                                  "en-US": title
+                                                                              },
+                                                                              "file": {
+                                                                                  "en-US": {
+                                                                                      "contentType": "image/png",
+                                                                                      "fileName": fileName,
+                                                                                      "uploadFrom": {
+                                                                                          "sys": {
+                                                                                              "type": "Link",
+                                                                                              "linkType": "Upload",
+                                                                                              "id": uploadId
+                                                                                          }
+                                                                                      }
+                                                                                  }
+                                                                              }
+                                                                          }
+                                                                      }
+                                                                      )
 
     return asset.process()
 
+
 if (mode == MODE_FILE):
-    asset=uploadAsset(file,args.title)
-    print(asset)
+    if (Path(file).suffix == '.md'):
+        print("Uploading content from: " + file)
+        # detecting frontmatter
+        fm = frontmatter.load(file)
+        if (len(fm.keys()) == 0):
+            print("FrontMatter not present in file.")
+        else:
+            title = fm['title']
+            print("FrontMatter: " + title)
+            print(uploadContent(title, (fm.content)))
+
+    else:
+        asset = uploadAsset(file, args.title)
+        print(asset)
 
 if (mode == MODE_DIR):
     files = os.listdir(dir)
-    #filtering by extension
+    # filtering by extension
     files = [f for f in files if Path(f).suffix in EXT_ALLOWED]
-    files = [os.path.join(dir,f) for f in files if os.path.isfile(os.path.join(dir,f))]
-    
+    files = [os.path.join(dir, f)
+             for f in files if os.path.isfile(os.path.join(dir, f))]
+
     print(files)
-    
+
     print("A total of %d files will be uploaded." % len(files))
     if input("Yes to continue? (y/n)") != "y":
         exit()
     for f in files:
-        asset=uploadAsset(f,args.title)
+        asset = uploadAsset(f, args.title)
         print(asset)
